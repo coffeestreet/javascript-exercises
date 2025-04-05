db89398211cab39296df7578f2cd638e7b4e8ce9
Rajveer Singh <king68rajveer62@gmail.com>
2025-04-05 14:27:07 +0530

    return the sum of all numbers between two given integers, inclusively

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
-};
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+      return "ERROR";
+    }
+  
+    const min = Math.min(a, b);
+    const max = Math.max(a, b);
+    let total = 0;
+  
+    for (let i = min; i <= max; i++) {
+      total += i;
+    }
+  
+    return total;
+  };
 
 // Do not edit below this line
 module.exports = sumAll;
